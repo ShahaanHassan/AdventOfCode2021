f2938fcdb868f303f196f67c977d365d3797c0a7
Shahaan,Hassan <shahaan.hassan@gmail.com>
2021-12-02 12:28:55 +0000

    add day2

---
+++ days/day2/day2.py
@@ -0,0 +1,41 @@
+from helpers.file_manager import read_file_as_list
+
+
+def split_commands(input_data: list) -> list:
+    return [command.split(' ') for command in input_data]
+
+
+def part_1(input_data: list) -> int:
+    horizontal, depth = 0, 0
+    for line in input_data:
+        command, distance = line.split(' ')
+        distance = int(distance)
+        if command == 'forward':
+            horizontal += distance
+        elif command == 'down':
+            depth += distance
+        elif command == 'up':
+            depth -= distance
+    return depth * horizontal
+
+
+def part_2(input_data: list) -> int:
+    horizontal, depth, aim = 0, 0, 0
+
+    for line in input_data:
+        command, distance = line.split(' ')
+        distance = int(distance)
+        if command == 'forward':
+            horizontal += distance
+            depth += distance * aim
+        elif command == 'down':
+            aim += distance
+        elif command == 'up':
+            aim -= distance
+    return depth * horizontal
+
+
+input_dir = 'day2/input.txt'
+data = read_file_as_list(input_dir)
+print(part_1(data))
+print(part_2(data))
