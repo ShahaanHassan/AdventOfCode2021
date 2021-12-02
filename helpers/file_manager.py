c9e82a907437c0fe5db2662f27625ed608fd43f9
Shahaan,Hassan <shahaan.hassan@gmail.com>
2021-12-02 12:28:40 +0000

    add lines reader

--- helpers/file_manager.py
+++ helpers/file_manager.py
@@ -2,11 +2,16 @@ from pathlib import Path
 import os
 
 
-def get_path_too_days(file_path: str) -> str:
+def get_path_to_days(file_path: str) -> str:
     root_path = Path(__file__).parent.parent
     return os.path.join(root_path, 'days', file_path)
 
 
+def read_file_as_list(path: str) -> list:
+    with open(get_path_to_days(path)) as file:
+        return file.readlines()
+
+
 def read_file_as_int_list(path: str) -> list:
-    with open(get_path_too_days(path)) as file:
+    with open(get_path_to_days(path)) as file:
         return [int(i) for i in file.readlines()]
