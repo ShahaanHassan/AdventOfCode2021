cd69340a624cb4b41475da9764e1769e74c54dd0
Shahaan,Hassan <shahaan.hassan@gmail.com>
2021-12-06 12:11:01 +0000

    minor refactor

--- days/day6/day6.py
+++ days/day6/day6.py
@@ -1,19 +1,17 @@
 from helpers.file_manager import read_lines_with_split_as_int
 
 
-def simulate_fish(fishes: list, days=18):
+def simulate_fish(fishes: list, days=18) -> int:
     population = [fishes.count(i) for i in range(0, 9)]
-
     print('day 0: {}'.format(population))
+
     for day in range(1, days + 1):
         new_population = [0] * 9
         for index in range(8, 0, -1):
             if index == 1:
                 new_population[6] += population[0]
                 new_population[8] = population[0]
-                new_population[0] = population[1]
-            else:
-                new_population[index - 1] = population[index]
+            new_population[index - 1] = population[index]
         population = new_population
         print('day {}: {}'.format(day, {i: x for i, x in enumerate(population)}))
     return sum(population)
