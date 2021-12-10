3bfe643ec55b165ac44ce90a82ad5c2c9e793e87
Shahaan,Hassan <shahaan.hassan@gmail.com>
2021-12-10 18:40:50 +0000

    clean up with recursion

--- days/day10/day10.py
+++ days/day10/day10.py
@@ -5,84 +5,60 @@ def create_brackets_map() -> dict:
     return {'(': ')', '[': ']', '{': '}', '<': '>'}
 
 
-def create__score_map() -> dict:
+def create_score_map() -> dict:
     return {')': 3, ']': 57, '}': 1197, '>': 25137}
 
 
-def create__score_map_2() -> dict:
+def create_score_map_2() -> dict:
     return {'(': 1, '[': 2, '{': 3, '<': 4}
 
 
-def calculuate_score(stack: list):
+def calculate_score(stack: list):
     score = 0
-    score_map = create__score_map_2()
+    score_map = create_score_map_2()
     for i in reversed(stack):
-        score *= 5
-        score += score_map.get(i)
+        score = (score * 5) + score_map.get(i)
     return score
 
 
-def check_lines(input_data: list):
-    brackets_map = create_brackets_map()
-    score_map = create__score_map()
-    final_score = 0
-    for line in input_data:
-        stack = [line[0]]
-        valid = True
-        pos = 1
-        score = 0
-        while stack or pos < len(line) and valid:
-            if pos > len(line)-1:
-                break
-            b = line[pos]
-            if b not in brackets_map.values():
-                stack.append(b)
-            else:
-                ob = stack.pop()
-                if not b == brackets_map.get(ob):
-                    score += score_map.get(b)
-                    valid = False
-            pos += 1
-        if not valid:
-            final_score += score
-    return final_score
+def check_line(line: list, stack: list, score_map: dict, brackets_map: dict, part: int):
+    if not line:
+        return 0 if part == 1 else calculate_score(stack)
+
+    cb = line.pop(0)
+    if cb not in brackets_map.values():
+        stack.append(cb)
+    else:
+        ob = stack.pop()
+        if not cb == brackets_map.get(ob):
+            return score_map.get(cb) if part == 1 else 0
+    return check_line(line, stack, score_map, brackets_map, part)
 
 
-def check_lines_2(input_data: list):
-    brackets_map = create_brackets_map()
-    score_map = create__score_map()
-    final_scores = []
-
-    for line in input_data:
-        stack = [line[0]]
-        valid = True
-        pos = 1
-        while stack or pos < len(line) and valid:
-            if pos > len(line)-1:
-                break
-            b = line[pos]
-            if b not in brackets_map.values():
-                stack.append(b)
-            else:
-                ob = stack.pop()
-                if not b == brackets_map.get(ob):
-                    valid = False
-            pos += 1
-        if valid:
-            final_scores.append(calculuate_score(stack))
-    a = sorted(final_scores)
-    return sorted(final_scores)[len(final_scores)//2]
+def check_lines(lines: list, score_map=create_score_map):
+    brackets_map, score_map = create_brackets_map(), score_map()
+    score = 0
+    for line in lines:
+        score += check_line(list(line)[1:], [line[0]], score_map, brackets_map, 1)
+    return score
 
 
-def part_1(input_data: list):
-    print(check_lines(input_data))
+def part_1(lines: list, score_map=create_score_map) -> int:
+    brackets_map, score_map = create_brackets_map(), score_map()
+    score = 0
+    for line in lines:
+        score += check_line(list(line)[1:], [line[0]], score_map, brackets_map, 1)
+    return score
 
 
-def part_2(input_data: list):
-    print(check_lines_2(input_data))
+def part_2(lines: list, score_map=create_score_map_2) -> int:
+    brackets_map, score_map = create_brackets_map(), score_map()
+    scores = [check_line(list(line)[1:], [line[0]], score_map, brackets_map, 2) for line in lines]
+    final_scores = list(filter(lambda x: x != 0, scores))
+    return sorted(final_scores)[len(final_scores) // 2]
 
 
 if __name__ == "__main__":
-    input_data = read_file_as_list('day10/input.txt')
-    # print(part_1(input_data))
-    print(part_2(input_data))
+    data = read_file_as_list('day10/input.txt')
+    print(part_1(data))
+    print(part_2(data))
